73696902df9cba5ebf4ee1561c342dfbc6bda9cb
jonahdh <jonahdh@email.unc.edu>
2022-11-04 20:26:25 -0400

    Addednum to prev commit, got rid of console logging protoypes

--- public/main.js
+++ public/main.js
@@ -20,7 +20,6 @@ const Buttons = {
   function navButton(div) {
     clearCurrentDiv();
     showDiv(div);
-    console.log("Active div: " + active);
   }
 
   // Take the current div and hide it
@@ -40,10 +39,8 @@ const multinav = document.getElementById("multinav");
 const guessnav = document.getElementById("guessnav");
 
 homenav.addEventListener("click", () => navButton(Buttons.Home));
-homenav.addEventListener("click", () => console.log("HOME BUTTON CLICKED!"));
 
 singlenav.addEventListener("click", () => navButton(Buttons.Single));
-singlenav.addEventListener("click", () => console.log("NAV BUTTON CLICKED!"));
 
 multinav.addEventListener("click", () => navButton(Buttons.Multi));
 
